7987a9f927a410fe5afed19058c17ccf3729c678
Jane <jane.e.nath@gmail.com>
2020-07-20 10:15:16 -0700

    set up react router

--- fibonacci_frontend/src/App.js
+++ fibonacci_frontend/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.scss';
+
+import Home from './Components/Home'
+import Error from './Components/Error'
+import Result from './Components/Result'
+
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
+    <Router>
+      <div className="appContainer">
+        <Switch>
+          <Route path="/error">
+            <Error/>
+          </Route>
+          <Route path="/result">
+            <Result/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+
   );
 }
 
